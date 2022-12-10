393c3e2e164ba0a8afefa4b17f493078e47909c7
agrawalrajat310 <agrawalrajat310@gmail.com>
2022-12-11 01:29:37 +0530

    2 files modified

--- index.js
+++ index.js
@@ -9,17 +9,20 @@ function Converter() {
     setKm(e.target.value);
   }
   function convert(km) {
-    return (km/1.609).toFixed(2);
+    return (km / 1.609).toFixed(2);
   }
 
-  return <div>
-  <input type="text" value={km} onChange={handleChange} />
-  <p> {km} km is {convert(km)} miles </p>
-  </div>;
+  return (
+    <div>
+      <input type="text" value={km} onChange={handleChange} />
+
+      <p>
+        {' '}
+        {km} km is {convert(km)} miles{' '}
+      </p>
+    </div>
+  );
 }
 
-const el = <Converter />; 
-ReactDOM.render(
-  el, 
-  document.getElementById('root')
-);
+const el = <Converter />;
+ReactDOM.render(el, document.getElementById('root'));
